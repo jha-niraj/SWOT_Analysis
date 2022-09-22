65546816774aaf1778a7e88d1b755c8312bbf653
Niraj Jha <109269916+jha-niraj@users.noreply.github.com>
2022-09-22 10:38:21 +0530

    Files added
    Here we added an files for the project.

---
+++ swot.js
@@ -0,0 +1,4 @@
+let hello = document.getElementById("btn2").addEventListener("click", function() {
+    let name = prompt("Can I know your Pretty name?");
+    alert("Hi, " + name + " how can I help you?");
+})
